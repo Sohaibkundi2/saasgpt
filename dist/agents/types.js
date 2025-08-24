6cc4cc77b962009b6522f009bd8199067f80cb8f
Sohaibkundi2 <sohaibkundi2@gmail.com>
2025-08-24 23:28:48 +0500

    feat: add frontend fix: refactor the structore

--- dist/agents/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.agentPlatform = void 0;
-var agentPlatform;
-(function (agentPlatform) {
-    agentPlatform["OPEN_AI"] = "open_ai";
-    agentPlatform["WRITING_ASSISTANT"] = "writing_assistant";
-})(agentPlatform || (exports.agentPlatform = agentPlatform = {}));
-//# sourceMappingURL=types.js.map
